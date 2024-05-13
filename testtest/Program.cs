11dcbb9247309beb996d1ca22c26aac2cdff8e7c
KrilynHje <157597213+KrilynHje@users.noreply.github.com>
2024-05-13 12:28:16 +0200

    Initial commit

---
+++ testtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
